dfddc60844a8ae4019c0e88776819c44d022d2aa
황석규 <hanmir91@gmail.com>
2019-02-26 01:26:53 +0900

    다음 사항 추가 - 레벨 - 경험치 - 종족 - 직업 - 전직 가능 직업

--- task1/src/main/java/com/common/Character.java
+++ task1/src/main/java/com/common/Character.java
@@ -1,19 +1,42 @@
 package com.common;
 
+import java.util.List;
+
 /**
  * 캐릭터
  */
 public class Character {
+    private int maxLevel;       // 최대 레벨
+    private int maxExp;         // 최대 경험치
     private int maxHp;          // 최대 HP
     private int maxMp;          // 최대 MP
+    private int level;          // 레벨
+    private int exp;            // 경험치
     private int hp;             // 현재 HP
     private int mp;             // 현재 MP
     private int attackPower;    // 공격력
     private int defensePower;   // 방어력
     private int dodgePower;     // 회피력
 
+    private Species species;                    // 종족
+    private CharClass charClass;                // 클래스
+    private List<CharClass> classChangeList;    // 전직 가능한 클래스
+
+    /**
+     * 생존 여부
+     * @return  {@code true}    생존
+     *          {@code false}   사망
+     */
     public boolean isAlive() {
         return hp > 0;
+    }
+
+    /**
+     * 최대 레벨에 도달했는지 체크
+     * @return
+     */
+    public boolean isMaxLevel() {
+        return level >= maxLevel;
     }
 
     /*
@@ -21,6 +44,22 @@ public class Character {
      |  Getter & Setter |
      +------------------+
      */
+    public int getMaxLevel() {
+        return maxLevel;
+    }
+
+    public void setMaxLevel(int maxLevel) {
+        this.maxLevel = maxLevel;
+    }
+
+    public int getMaxExp() {
+        return maxExp;
+    }
+
+    public void setMaxExp(int maxExp) {
+        this.maxExp = maxExp;
+    }
+
     public int getMaxHp() {
         return maxHp;
     }
@@ -35,6 +74,22 @@ public class Character {
 
     public void setMaxMp(int maxMp) {
         this.maxMp = maxMp;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
     }
 
     public int getHp() {
@@ -75,5 +130,29 @@ public class Character {
 
     public void setDodgePower(int dodgePower) {
         this.dodgePower = dodgePower;
+    }
+
+    public Species getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(Species species) {
+        this.species = species;
+    }
+
+    public CharClass getCharClass() {
+        return charClass;
+    }
+
+    public void setCharClass(CharClass charClass) {
+        this.charClass = charClass;
+    }
+
+    public List<CharClass> getClassChangeList() {
+        return classChangeList;
+    }
+
+    public void setClassChangeList(List<CharClass> classChangeList) {
+        this.classChangeList = classChangeList;
     }
 }
